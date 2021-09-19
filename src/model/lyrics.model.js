0c28776928134aaa4ab189cf82e2b4e9d17f9eda
BhagyaGunathilaka <2018cs059@stu.ucsc.cmb.ac.lk>
2021-09-19 23:02:16 +0530

    lyrics model changes

--- src/model/lyrics.model.js
+++ src/model/lyrics.model.js
@@ -11,15 +11,33 @@ var Lyrics = function (lyrics) {
 };
 
 Lyrics.getLyrics = (result) => {
-	dbConn.query("SELECT * FROM lyrics WHERE ActiveState=1", (err, res) => {
-		if (err) {
-			console.log("Error while fetching lyrics", err);
-			result(null, err);
-		} else {
-			console.log("Lyrics fetched succesfully", err);
-			result(null, res);
+	dbConn.query(
+		"SELECT * FROM lyrics JOIN user ON lyrics.UserId=user.UserId WHERE ActiveState=1",
+		(err, res) => {
+			if (err) {
+				console.log("Error while fetching lyrics", err);
+				result(null, err);
+			} else {
+				console.log("Lyrics fetched succesfully", err);
+				result(null, res);
+			}
 		}
-	});
+	);
 };
+
+// Lyrics.getCounts = (result) => {
+// 	dbConn.query(
+// 		"SELECT (SELECT COUNT(user.UserId) FROM user ) AS usercnt, (SELECT (SELECT COUNT(cover.CoverId) FROM cover)+(SELECT COUNT(lyrics.LyricId) FROM lyrics))  AS posts",
+// 		(err, res) => {
+// 			if (err) {
+// 				console.log("Error while fetching counts", err);
+// 				result(null, err);
+// 			} else {
+// 				console.log("Counts fetched succesfully", err);
+// 				result(null, res);
+// 			}
+// 		}
+// 	);
+// };
 
 module.exports = Lyrics;
